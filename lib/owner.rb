db7a94a9c3da51ec37fd3968cd0decf7aba28b45
Ashley Norton <nortonashleym@gmail.com>
2019-12-16 03:17:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = [] 
+  @@count = 0 
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+    @@count += 1 
+  end 
+  
+  def say_species
+    "I am a #{species}."
+  end 
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.count
+    @@count 
+  end 
+  
+  def self.reset_all
+    self.all.clear
+    @@count =  0 
+  end 
 end
